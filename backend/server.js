5ea7c8ad24182d4d4e5644806208ca96370063f7
Ajai Sharan <ajaisharan2020@gmail.com>
2025-03-20 22:43:24 +0530

    Added code in frontent

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
-import path from 'path';
 import productRouter from './route/product.route.js';
 
 dotenv.config();
@@ -12,20 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const __dirname = path.resolve();
-
-
 
 app.use('/api/product', productRouter);
 
-
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+connectDB();
 
 export default app;
